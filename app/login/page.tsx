305b69b10a451b03d0e4cb12d4a97ceb7e91cb6d
jordilaurin30-tech <jordilaurin30@gmail.com>
2026-03-08 13:17:14 +0100

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -925,10 +925,4 @@ function DobBox({
     </div>
   );
 }
-export default function LoginPage() {
-  return (
-    <Suspense fallback={<div style={{ padding: 24 }}>Lade Login…</div>}>
-      <LoginPageInner />
-    </Suspense>
-  );
-}
+
